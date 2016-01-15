aa1ecdedec65efcea4c39a837c164d79a382cca2
Baocheng Sun <baocheng.sun@amlogic.com>
2016-01-15 10:35:11 +0800

    pd#118050:format external storage use disk change event
    Revert "pd#115856: don't deal with disk change event"
    This reverts commit a25eb421bcfa83129b616cc21434e13d6360b9f3.
    
    Change-Id: Id709225c3e4f9a4a09bca71ee1c3afac88bfd28b

--- VolumeManager.cpp
+++ VolumeManager.cpp
@@ -322,15 +322,12 @@ void VolumeManager::handleBlockEvent(NetlinkEvent *evt) {
     }
     case NetlinkEvent::Action::kChange: {
         LOG(DEBUG) << "Disk at " << major << ":" << minor << " changed";
-        LOG(DEBUG) << "we do not deal with disk change";
-        /*
         for (auto disk : mDisks) {
             if (disk->getDevice() == device) {
                 disk->readMetadata();
                 disk->readPartitions();
             }
         }
-        */
         break;
     }
     case NetlinkEvent::Action::kRemove: {
